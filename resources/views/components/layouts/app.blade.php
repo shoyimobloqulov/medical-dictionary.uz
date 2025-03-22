e9ff0a0bc456bf8695264c7469b60f491bc3b7b6
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2025-03-22 13:40:12 +0500

    tailwind, livewire: fixed code

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!-- resources/views/components/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'My App' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="bg-gray-100">
+<div class="container mx-auto p-4">
+    {{ $slot }}
+</div>
+@livewireScripts
+</body>
+</html>
